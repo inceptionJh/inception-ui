ab66d2e636b29db128ac3ce95bf8bec2cbcc2a18
jaehunkim <jaehunkim7730@gmail.com>
2019-12-03 13:18:28 +0900

    Create Chart Line Shape Component

--- src/Chart/Shape/index.ts
+++ src/Chart/Shape/index.ts
@@ -1,5 +1,7 @@
 import Bar from "./Bar";
+import Line from "./Line";
 
 export default {
   Bar,
+  Line,
 };
--- test/index.tsx
+++ test/index.tsx
@@ -21,7 +21,7 @@ const data = [
   { date: new Date(`2018-01-01`), price: 100000000, amount: 1 },
   { date: new Date(`2018-02-01`), price: 120000000, amount: 2 },
   { date: new Date(`2018-03-01`), price: 140000000, amount: 6 },
-  // { date: new Date(`2018-04-01`), price: 130000000, amount: 6 },
+  { date: new Date(`2018-04-01`), price: 160000000, amount: 6 },
   { date: new Date(`2018-05-01`), price: 170000000, amount: 5 },
   { date: new Date(`2018-06-01`), price: 180000000, amount: 2 },
 ].map((v) => ({ ...v, date: v.date.valueOf() }));
@@ -48,15 +48,15 @@ const Test = () => {
     return dateHelper.generateDateList(new Date(data[0].date), new Date(data[data.length - 1].date), "month").map((v) => v.valueOf());
   }, [data]);
 
-  const yMax = d3.max(data.map((v) => v.price));
-  const yDiff = yMax - 0;
-  const yGap = Math.round(yDiff * 0.1 / Math.pow(10, `${yDiff}`.length - 2)) * Math.pow(10, `${yDiff}`.length - 2);
-  const yDomain = [yMax + yGap, 0] as [number, number];
+  const [yMin, yMax] = d3.extent(data.map((v) => v.price));
+  const yDiff = yMax - yMin;
+  const yGap = Math.round(yDiff * 0.2 / Math.pow(10, `${yDiff}`.length - 2)) * Math.pow(10, `${yDiff}`.length - 2);
+  const yDomain = [yMax + yGap, yMin - yGap] as [number, number];
   const yRange = [padding.top, 500 - padding.bottom];
 
   const yTicks = React.useMemo(() => {
     const ticks = [] as number[];
-    for (let tick = 0; tick <= yDomain[0];) {
+    for (let tick = yDomain[1]; tick <= yDomain[0];) {
       ticks.push(tick);
       tick += yGap;
     }
@@ -73,7 +73,7 @@ const Test = () => {
       height={500}
       data={data}
     >
-      <Chart.Shape.Bar
+      <Chart.Shape.Line
         xKey="date"
         yKey="price"
         xScale={xScale}
@@ -92,7 +92,7 @@ const Test = () => {
           ticks={yTicks}
         />
         <Chart.Tooltip component={Tootip} />
-      </Chart.Shape.Bar>
+      </Chart.Shape.Line>
     </Chart.Area>
   );
 };
