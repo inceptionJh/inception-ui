26b024f6447a7a7fddf5a0f20170f9b18f6a7662
jaehunkim <jaehunkim7730@gmail.com>
2019-12-10 18:16:24 +0900

    Modify Portal lifecycle

--- src/Chart/Shape/Bar/index.tsx
+++ src/Chart/Shape/Bar/index.tsx
@@ -40,14 +40,6 @@ const Bar: React.FunctionComponent<IBarProps> = (props) => {
   const width = areaCtx.width - (areaCtx.padding.left + areaCtx.padding.right);
   const height = areaCtx.height - (areaCtx.padding.top + areaCtx.padding.bottom);
 
-  const [render, setRender] = React.useState(false);
-
-  React.useEffect(() => {
-    setRender(true);
-  }, []);
-
-  if (!render) return null;
-
   return (
     <ShapeContext.Provider
       value={{
